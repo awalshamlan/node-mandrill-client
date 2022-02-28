0be20d5992a3d9fb51fbe572c6683336af972207
ocean-rey <x7flux@gmail.com>
2022-02-28 12:14:35 +0300

    updated binaries

--- lib/esm/index.d.ts
+++ lib/esm/index.d.ts
@@ -10,11 +10,10 @@ interface SendEmailArgs {
     };
     subject: string;
 }
-export declare class MailClient {
+export default class MailClient {
     #private;
     constructor(apiKey: string);
     sendEmail({ recepient, variables, template, from, subject }: SendEmailArgs): Promise<import("axios").AxiosResponse<any, any>>;
     scheduleEmail(): void;
 }
-declare const _default: MailClient;
-export default _default;
+export {};
